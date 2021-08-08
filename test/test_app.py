3c36d0d7270e7555962731e6eb3573fbec116be7
Salvador HM <salvadorhm@gmail.com>
2021-08-07 20:49:51 -0500

    webtestfrom webtest import TestApp as ta

--- test/test_app.py
+++ test/test_app.py
@@ -1,11 +1,11 @@
-from webtest import TestApp
-from app import app
+from webtest import TestApp as ta
+from app import app as application
 
 class TestIndex():
     def test_get(self):
         middleware = []
-        a = TestApp(app.wsgifunc(*middleware))
-        resp = a.get('/index')
+        app = ta(application.wsgifunc(*middleware))
+        resp = app.get('/index')
         print(resp)
         assert resp.status == '200 OK'
         assert resp.text == 'Hello, world!'
@@ -14,8 +14,8 @@ class TestDato():
 
     def test_get(self):
         middleware = []
-        a = TestApp(app.wsgifunc(*middleware))
-        resp = a.get('/dato')
+        app = ta(application.wsgifunc(*middleware))
+        resp = app.get('/dato')
         print(resp)
         assert resp.status == '200 OK'
         assert resp.text == 'Hello, world!'
@@ -23,8 +23,8 @@ class TestDato():
 
     def test_post(self):
         middleware = []
-        a = TestApp(app.wsgifunc(*middleware))
-        resp = a.post('/dato',{'name':'John Doe'})
+        app = ta(application.wsgifunc(*middleware))
+        resp = app.post('/dato',{'name':'John Doe'})
         print(resp)
         assert resp.status == '200 OK'
         assert resp.text == 'Hello, John Doe!'
